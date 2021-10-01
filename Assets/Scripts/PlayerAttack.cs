eb2d7d195bc0e0d84058f2ef1587ccd8ff51aa52
Santiago Herrera Panizzo <smhp-1104@hotmail.com>
2021-10-01 11:26:21 -0500

    Test de Ataque simple con animacion de prueba Player

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator playeranim;
+    public float timer;
+    public float attacktimer;
+    public bool attacking;
+
+
+    void Start()
+    {
+        attacktimer = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        attacktimer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+            playeranim.SetBool("Attack", true);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && attacktimer >= 0.7)
+        {
+            
+            attacktimer = 0;
+        }
+
+        if (attacktimer >= 0.5)
+        {
+            playeranim.SetBool("Attack", false);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+
+            playeranim.SetBool("Attack", false);
+
+        }
+    }
+
+
+}
