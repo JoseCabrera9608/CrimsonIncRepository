2f43b858009841cad46ef7600262cd3333c31038
JoseCabrera9608 <73959334+JoseCabrera9608@users.noreply.github.com>
2021-10-13 14:29:05 -0500

    Persecución boss 3 y base de habilidad 1 y 2
    Codeo de habilidades pero sin animación

--- Assets/Scripts/JoseScripts/CalamarController.cs
+++ Assets/Scripts/JoseScripts/CalamarController.cs
@@ -6,33 +6,66 @@ using UnityEngine.AI;
 
 public class CalamarController : MonoBehaviour
 {
+    [SerializeField] private bool onChase = false;
+    [SerializeField] private float timerCooldown;
+    [SerializeField] private bool canCast;
+    [SerializeField] private float distance;
+    //public CalamarVisuals calamarAnimations;
     GameObject player;
     private NavMeshAgent agente;
-    private bool onChase = false;
-    AudioSource growl;
-    private bool growling = false;
+    public bool electrocutado;
+
+
+
     void Start()
     {
         BossGameEVent.current.combatTriggerExit += StartChase; //El metodo se suscribe al BossGameEvent
         agente = GetComponent<NavMeshAgent>();
-        growl = GetComponent<AudioSource>();
         player = GameObject.FindWithTag("Player");
-        growl.Stop();
     }
     private void Update()
     {
-        if (onChase == true)
+
+
+        if (onChase == true) // Cuando se activa el boss
         {
             agente.SetDestination(player.transform.position);
-            
+
+            distance = Vector3.Distance(transform.position, player.transform.position);
+            timerCooldown += Time.deltaTime;
+            if (timerCooldown >= 5f)
+            {
+                canCast = true;
+                timerCooldown = 0;
+
+            }
+            if (distance >= 5 && canCast == true)
+            {
+
+                StartCoroutine(TentacleStrike());
+                canCast = false;
+            }
+
+            if (distance >= 2 && canCast == true)
+            {
+                electrocutado = true;
+            }
+
         }
-      
     }
     private void StartChase()
     {
         onChase = true;
-        growl.Play();
     }
+
+    IEnumerator TentacleStrike()
+    {
+        agente.speed = 0;
+        //calamarAnimations.TentacleStrikeAnimation();
+        yield return new WaitForSeconds(3);
+        agente.speed = 3.5f;
+    }
+
 
     private void OnDestroy() //Metodo por si se destruye el objeto se desuscriba del BossGameEvent para evitar errores
     {
