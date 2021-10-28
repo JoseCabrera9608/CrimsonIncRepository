e45ebcc4b6b0892bf3815cd3107c23e7deb65f00
Santiago Herrera Panizzo <smhp-1104@hotmail.com>
2021-10-28 17:57:32 -0500

    Animacion de movimiento New Player

--- Assets/Inputs/InputManager.cs
+++ Assets/Inputs/InputManager.cs
@@ -11,6 +11,13 @@ public class InputManager : MonoBehaviour
     public float verticalInput;
     public float horizontalInput;
 
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     private void OnEnable()
     {
         if (playerControls == null)
@@ -22,6 +29,18 @@ public class InputManager : MonoBehaviour
 
         playerControls.Enable();
 
+    }
+
+    private void Update()
+    {
+        if (verticalInput != 0 || horizontalInput != 0)
+        {
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
     }
 
     private void OnDisable()
