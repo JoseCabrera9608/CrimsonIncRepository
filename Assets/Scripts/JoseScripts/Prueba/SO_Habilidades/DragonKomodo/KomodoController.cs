0060cae5dc4468d9f0d762db10bcc699cf6932b2
JoseCabrera9608 <73959334+JoseCabrera9608@users.noreply.github.com>
2021-10-29 22:09:41 -0500

    Habilidad Komodo

--- Assets/Scripts/JoseScripts/Prueba/SO_Habilidades/DragonKomodo/KomodoController.cs
+++ Assets/Scripts/JoseScripts/Prueba/SO_Habilidades/DragonKomodo/KomodoController.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class KomodoController : MonoBehaviour
 {
     public bool lanzamientoMisiles = false;
+    public GameObject nube;
+    public bool lanzarNube;
+    Transform target;
+    GameObject player;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        target = player.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.LookAt(target);
+        if(lanzarNube == true)
+        {
+            StartCoroutine(LanzarNube());
+            lanzarNube = false;
+        }
         
+    }
+
+    IEnumerator LanzarNube()
+    {
+        nube.SetActive(true);
+        yield return new WaitForSeconds(8);
+        nube.SetActive(false);
     }
 }
--- Assets/Scripts/JoseScripts/Prueba/SO_Habilidades/DragonKomodo/NubeCorrosiva.cs
+++ Assets/Scripts/JoseScripts/Prueba/SO_Habilidades/DragonKomodo/NubeCorrosiva.cs
@@ -5,31 +5,11 @@ using UnityEngine;
 [CreateAssetMenu]
 public class NubeCorrosiva : Habilidad_SO
 {
-    GameObject nube;
-    float timer;
-    bool activar = false;
-
+    KomodoController _komodoController;
     public override void Activate(GameObject parent)
     {
-        nube = GameObject.FindGameObjectWithTag("Nube");
-        timer += Time.deltaTime;
-        if(timer >= 1)
-        {
-            activar = true;
-            if(timer >= 4)
-            {
-                activar = false;
-                timer = 0;
-            }
-        }
-        if(activar == true)
-        {
-            nube.SetActive(true);
-        }
-        if(activar == false)
-        {
-            nube.SetActive(false);
-        }
+        _komodoController = parent.GetComponent<KomodoController>();
+        _komodoController.lanzarNube = true;
     }
 
 }
