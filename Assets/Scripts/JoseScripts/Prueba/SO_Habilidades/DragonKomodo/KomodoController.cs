f46b66ae765063baf242dfdc2560e2ee63c6dc28
JoseCabrera9608 <73959334+JoseCabrera9608@users.noreply.github.com>
2021-10-29 22:45:12 -0500

    Komodo avance

--- Assets/Scripts/JoseScripts/Prueba/SO_Habilidades/DragonKomodo/KomodoController.cs
+++ Assets/Scripts/JoseScripts/Prueba/SO_Habilidades/DragonKomodo/KomodoController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class KomodoController : MonoBehaviour
 {
+    float timer;
+    bool startFight;
     public bool lanzamientoMisiles = false;
     public GameObject nube;
     public bool lanzarNube;
@@ -13,12 +15,19 @@ public class KomodoController : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player");
         target = player.transform;
+        BossGameEVent.current.combatTriggerExit += FightStart;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        
+        if (startFight == true)
+        {
+           
+            transform.LookAt(target);
+        }
+        
         if(lanzarNube == true)
         {
             StartCoroutine(LanzarNube());
@@ -32,5 +41,18 @@ public class KomodoController : MonoBehaviour
         nube.SetActive(true);
         yield return new WaitForSeconds(8);
         nube.SetActive(false);
+    }
+    IEnumerator RotacionInicial()
+    {
+        transform.Rotate(new Vector3(0f, -90f, 0f) * Time.deltaTime);
+        yield return new WaitForSeconds(3);
+        startFight = true;
+    }
+
+    void FightStart()
+    {
+        //startFight = true;
+        StartCoroutine(RotacionInicial());
+        
     }
 }
