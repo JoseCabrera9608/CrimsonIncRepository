3ebf606dd11819409c4f83c4c69968594396dd68
JoseCabrera9608 <73959334+JoseCabrera9608@users.noreply.github.com>
2021-11-26 20:13:40 -0500

    Ajuste Misil Komodo

--- Assets/Scripts/JoseScripts/Misile.cs
+++ Assets/Scripts/JoseScripts/Misile.cs
@@ -7,23 +7,32 @@ public class Misile : MonoBehaviour
     Transform target;
     GameObject player;
     GameObject cabezaPlayer;
-    float speed = 10f;
+    [SerializeField] float speed = 15f;
     PlayerStats playerStats;
+    [SerializeField] private float impulso;
+    private bool perseguir = false;
+    private Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
         player = GameObject.FindGameObjectWithTag("Player");
         playerStats = player.GetComponent<PlayerStats>();
         cabezaPlayer = GameObject.FindGameObjectWithTag("PlayerCabeza");
         target = cabezaPlayer.transform;
+        StartCoroutine(Despegue());
     }
 
     // Update is called once per frame
-    void Update()
+    void  FixedUpdate()
     {
-        transform.LookAt(target.position);
-        transform.Translate(0f, 0f, speed * Time.deltaTime);
-        Destroy(gameObject, 6f);
+
+        if (perseguir)
+        {
+            transform.LookAt(target.position);
+            transform.Translate(0f, 0f, speed * Time.deltaTime);
+            Destroy(gameObject, 10f);
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -37,5 +46,11 @@ public class Misile : MonoBehaviour
         }
         
     }
+    IEnumerator Despegue()
+    {
+        rb.AddForce(Vector3.up * impulso, ForceMode.Impulse);
+        yield return new WaitForSeconds(1.6f);
+        perseguir = true;
 
+    }
 }
