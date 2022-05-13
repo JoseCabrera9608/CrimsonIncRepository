689739a43289a76beb6b6992584f36c6ac36c85b
Santiago Herrera Panizzo <smhp-1104@hotmail.com>
2022-05-13 13:32:52 -0500

    vida cangrejo

--- Assets/JoseScripts/CangrejoUI.cs
+++ Assets/JoseScripts/CangrejoUI.cs
@@ -15,7 +15,7 @@ public class CangrejoUI : MonoBehaviour
     void Start()
     {
         enemy = Enemy.gameObject.GetComponent<BossCangrejo>();
-        enemy.vidaActual = 10;
+        enemy.vidaActual = 150;
     }
 
     // Update is called once per frame
