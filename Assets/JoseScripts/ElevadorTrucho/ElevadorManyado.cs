a22d9620f65d5b76e1c77979de896e52b51b13e6
JoseCabrera9608 <Jecc9608@gmail.com>
2022-05-20 14:23:28 -0500

    ElevadorFix

--- Assets/JoseScripts/ElevadorTrucho/ElevadorManyado.cs
+++ Assets/JoseScripts/ElevadorTrucho/ElevadorManyado.cs
@@ -77,7 +77,7 @@ public class ElevadorManyado : MonoBehaviour
     }
     public void DetenerElevador()
     {
-        
+        barrera.SetActive(false);
         StopAllCoroutines();
     }
     public void BajarElevadorMetodo()
