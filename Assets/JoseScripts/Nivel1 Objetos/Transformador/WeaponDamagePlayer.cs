ebd539ad09906eab74191b2135a3f9d2cd4ec224
JoseCabrera9608 <Jecc9608@gmail.com>
2022-05-22 23:32:01 -0500

    Transformadores

---
+++ Assets/JoseScripts/Nivel1 Objetos/Transformador/WeaponDamagePlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDamagePlayer : MonoBehaviour
+{
+    public int dañoDeArma;
+    public int dañoDeArmaPasiva;
+    public bool hitted;
+    //public GameObject efecto;
+    //public GameObject puntadeLanza;
+
+   
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("CuerpoBoss"))
+        {  
+            hitted = true;
+           // Quaternion rotation = Quaternion.FromToRotation(Vector3.up, puntadeLanza.transform.localPosition);
+           // Instantiate(efecto, puntadeLanza.transform.position, rotation);
+        }
+       
+    }
+
+   
+
+}
