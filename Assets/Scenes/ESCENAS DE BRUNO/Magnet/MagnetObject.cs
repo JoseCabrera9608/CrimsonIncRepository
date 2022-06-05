478e27396a63bf0902264b2deda432e368f4743f
Santiago Herrera Panizzo <smhp-1104@hotmail.com>
2022-06-05 01:27:49 -0500

    Update MagnetObject.cs

--- Assets/Scenes/ESCENAS DE BRUNO/Magnet/MagnetObject.cs
+++ Assets/Scenes/ESCENAS DE BRUNO/Magnet/MagnetObject.cs
@@ -26,7 +26,7 @@ public class MagnetObject : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         StartAtraction();
         PullPlayer();
