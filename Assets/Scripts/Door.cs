be74eb87f02b6c63aab743b67b75bbd85e120c74
Santiago Herrera Panizzo <smhp-1104@hotmail.com>
2022-06-08 20:36:33 -0500

    gaaadoor

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; 
 
 public class Door : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Door : MonoBehaviour
     public bool bossDoor;
     public BoxCollider triggerbox;
     public int enemysToOpen;
+    public TextMeshPro text;
 
     public ProgressManager progress;
     
@@ -17,15 +19,21 @@ public class Door : MonoBehaviour
     void Start()
     {
         progress = GameObject.FindGameObjectWithTag("Progress").GetComponent<ProgressManager>();
+        text = GetComponentInChildren<TextMeshPro>();
+        text.text = "Enemigos derrotados: 0/"+ enemysToOpen;
     }
 
     // Update is called once per frame
     void Update()
     {
+        text.text = "Enemigos derrotados: "+ progress.enemysdeath +"/" + enemysToOpen;
+
+
         if (progress.enemysdeath >= enemysToOpen && enemysToOpen != 0)
         {
+            text.enabled = false;
             OpenDoor();
-            //progress.enemysdeath = 0;
+            progress.enemysdeath = 0;
         }
     }
 
