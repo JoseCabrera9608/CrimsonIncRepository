f579b6ed867813e6c3eb533ed453f671e568a3e2
JoseCabrera9608 <Jecc9608@gmail.com>
2022-06-10 13:57:50 -0500

    Lvl2 Ajustes lasers sondas y activador

---
+++ Assets/JoseScripts/Activador.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool colliding;
+    public PlayerStatus playerStatus;
+    public GameObject Player;
+    public Transform consolepoint;
+    public GameObject Puerta;
+    Animator puertaAnim;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        playerStatus = Player.GetComponent<PlayerStatus>();
+        puertaAnim = Puerta.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && colliding == true)
+        {
+            //progress.checkpointIndex = checkpointIndex;
+            //progress.lastposition = SpawnPoint.transform.position;
+            FindObjectOfType<AudioManager>().Play("Checkpoint");
+
+
+            playerStatus.InteractualObject = consolepoint;
+            playerStatus.Interacting();
+            puertaAnim.SetTrigger("Activado");
+            
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            colliding = true;
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            colliding = false;
+        }
+    }
+}
