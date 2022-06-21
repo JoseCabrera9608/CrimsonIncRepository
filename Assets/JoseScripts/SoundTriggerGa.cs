b759de23a3750858808bae48931b3e73ee95c9b7
JoseCabrera9608 <Jecc9608@gmail.com>
2022-06-21 12:37:22 -0500

    Ga

--- Assets/JoseScripts/SoundTriggerGa.cs
+++ Assets/JoseScripts/SoundTriggerGa.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class SoundTriggerGa : MonoBehaviour
 {
     public string nombreCancion;
+    public string cancionAnterior;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            FindObjectOfType<AudioManager>().Stop(cancionAnterior);
             FindObjectOfType<AudioManager>().Play(nombreCancion);
             Destroy(this.gameObject);
+            
         }
         
         
